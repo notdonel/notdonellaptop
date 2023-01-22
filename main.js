19e9947e1527b3dc52b62c9ff097cde525b5363e
Don'EL <106171595+notdonel@users.noreply.github.com>
2023-01-22 18:29:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0)=== "R")
+    return name+"plays banjo"
+    else 
+      return name+"does not play banjo"
+  }
+
+areYouPlayingBanjo("Ron")
